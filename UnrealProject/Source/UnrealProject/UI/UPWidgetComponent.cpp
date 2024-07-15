db76deefd21068b8d76df492762ae55136c9f640
수아 <79245006+Kimsua99@users.noreply.github.com>
2024-07-15 20:23:43 +0900

    Update UPWidgetComponent.cpp

--- UnrealProject/Source/UnrealProject/UI/UPWidgetComponent.cpp
+++ UnrealProject/Source/UnrealProject/UI/UPWidgetComponent.cpp
@@ -2,4 +2,15 @@
 
 
 #include "UI/UPWidgetComponent.h"
+#include "UPUserWidget.h"
 
+void UUPWidgetComponent::InitWidget()//BeginPlay가 호출되면 이 함수가 호출되는데, 이때 내부의 CreateWidget 함수를 통해 위젯이 생성된다. 이후 NativeConstruct 함수가 실행된다.
+{
+	Super::InitWidget();
+
+	UUPUserWidget* UPUserWidget = Cast<UUPUserWidget>(GetWidget());//인스턴스화 되어 위젯 생성 완료되었으므로 캐스팅해서 위젯 받아올 수 있음.
+	if (UPUserWidget)
+	{
+		UPUserWidget->SetOwningActor(GetOwner());//자신을 소유하고 있는 액터 정보를 얻어옴.
+	}
+}
